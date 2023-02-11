c4b3427612db51a800c8c3b64baeb88e37a57224
Rômulo Silva <romulo.sm15@gmail.com>
2023-02-11 01:25:29 -0300

    logo, footer rainbow and skills added

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  colors: string[] = ['#010101', '#603814', '#75D6EC', '#FEAFC7', '#760188', '#011A98', '#06BD02', '#FFFF00', '#FE941D', '#FF1D25']
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
   links = [
     { label: 'Início', href: ['/'] },
@@ -14,5 +14,42 @@ export class HeaderComponent implements OnInit {
     { label: 'Certificados', href: ['#certified'] },
   ];
 
-  ngOnInit(): void {}
+  skill = '';
+  skills = ['frontend developer', 'UX designer like', 'responsive UI', 'angular components'];
+  intervalToText!: any
+
+  changeSkillOnHero() {
+    let interval = 5000;
+    let promise = Promise.resolve();
+
+    this.skills.forEach((string) => {
+      promise = promise.then(() => {
+
+        let promiseChar = Promise.resolve();
+
+        string.split('').forEach((char) => {
+
+          promiseChar = promiseChar.then(() => {
+            this.skill = this.skill.concat(char);
+
+            return new Promise((resolveChar) => {
+              setTimeout(resolveChar, string.length * 6);
+            });
+          });
+        })
+
+        this.skill = ''
+
+        return new Promise((resolve) => {
+          setTimeout(resolve, interval);
+        });
+      });
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.changeSkillOnHero();
+  }
 }
+
