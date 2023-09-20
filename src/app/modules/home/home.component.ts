0e1d65eeaedcff0be090d2613397dcd4ca1e3e72
Rômulo Silva <romulo.sm15@gmail.com>
2023-09-19 23:32:53 -0300

    feat: components created, dimensions file added and refactoring some methods

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,20 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
-import { projects } from './../project/list/list.component';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  skills = ['angular', 'css 3', 'UX Designer', 'HTML5'];
-  projects = projects;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HomeComponent { }
