8941973d5909ef176b669a8654e7270fea1c9bff
Rômulo Silva <romulo.sm15@gmail.com>
2024-05-04 14:05:13 -0300

    chore: build of application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { Injectable, NgModule } from '@angular/core';
-import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
+import {
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+  TitleStrategy,
+} from '@angular/router';
 
 import { HomeComponent } from './modules/home/home.component';
 import { Title } from '@angular/platform-browser';
@@ -23,7 +28,7 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: HomeComponent,
-    title: 'Portifólio',
+    title: 'Perfil profissional e portfólio',
   },
   /* {
     path: 'projects',
@@ -39,8 +44,8 @@ const routes: Routes = [
   providers: [
     {
       provide: TitleStrategy,
-      useClass: TemplatePageTitleStrategy
-    }
-  ]
+      useClass: TemplatePageTitleStrategy,
+    },
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
